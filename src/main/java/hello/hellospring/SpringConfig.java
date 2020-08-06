cc68be5bd1f58351f3a33a62d6fb3b089e90852a
Yoonminsu <yoonminsu97@gmail.com>
2020-08-06 17:20:54 +0900

    JPA 예제 연습

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,27 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import javax.swing.text.html.parser.Entity;
 
 @Configuration
 public class SpringConfig {
 
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig( EntityManager em ) {
+        this.em = em;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService( memberRepository() );
@@ -16,6 +29,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JpaMemberRepository( em );
     }
 }
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -55,13 +55,6 @@ class MemberServiceTest {
         memberService.join( member1 );
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat( e.getMessage()).isEqualTo( "이미 존재하는 회원입니다.");
-//       try {
-//            memberService.join( member2 );
-//            fail();
-//        } catch ( IllegalStateException e ) {
-//            Assertions.assertThat( e.getMessage()).isEqualTo( "이미 존재하는 회원입니다." );
-//        }
-//        // then
     }
 
     @Test
